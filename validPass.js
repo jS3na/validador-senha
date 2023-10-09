4c990970b5ac6e86b7f900ce17cc52d3706ae39b
João Sena <126720426+jS3na@users.noreply.github.com>
2023-10-08 23:39:16 -0300

    Validação Senha - Create Func

--- validPass.js
+++ validPass.js
@@ -1,37 +1,42 @@
 var senha = prompt("Digite uma senha:");
-var countM = false;
-var countNum = false;
+Validar(senha);
 
-if (senha.length < 8) {
-    console.log("Não contém 8 caracteres ou mais");
-} 
-
-else {
-    console.log("Contém 8 caracteres ou mais");
-}
-
-for (var i = 0; i < senha.length; i++) {
-    if (/[A-Z]/.test(senha[i])) {
-        countM = true;
+function Validar(sen){
+    var countM = false;
+    var countNum = false;
+    
+    if (sen.length < 8) {
+        console.log("Não contém 8 caracteres ou mais");
+    } 
+    
+    else {
+        console.log("Contém 8 caracteres ou mais");
     }
     
-    if (!isNaN(senha[i])) {
-        countNum = true;
+    for (var i = 0; i < sen.length; i++) {
+        if (/[A-Z]/.test(sen[i])) {
+            countM = true;
+        }
+        
+        if (!isNaN(sen[i])) {
+            countNum = true;
+        }
     }
-}
+    
+    if (countM) {
+        console.log("Tem ao menos uma letra maiúscula");
+    } 
+    
+    else {
+        console.log("Não tem nenhuma letra maiúscula");
+    }
+    
+    if (countNum) {
+        console.log("Tem ao menos um número");
+    }
+    
+    else {
+        console.log("Não tem nenhum número");
+    }
 
-if (countM) {
-    console.log("Tem ao menos uma letra maiúscula");
-} 
-
-else {
-    console.log("Não tem nenhuma letra maiúscula");
-}
-
-if (countNum) {
-    console.log("Tem ao menos um número");
-}
-
-else {
-    console.log("Não tem nenhum número");
 }
